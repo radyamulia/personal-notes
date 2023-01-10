5fb37b57258ddbbef042b1b15dca22260fb3a35e
Radya Mulia <radyamulia@gmail.com>
2023-01-10 22:31:32 +0800

    adds icons, prop-types and router

--- src/components/DeleteButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
- 
-function DeleteButton({ id, onDelete }) {
-  return <button className='btn-item btn-item__delete' onClick={() => onDelete(id)}>Delete</button>
-}
- 
-export default DeleteButton;
